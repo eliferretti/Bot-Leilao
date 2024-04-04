d538b0892b7371a9f393c68a9953c1e2e23d0779
eliferretti <eliferretti14@gmail.com>
2024-04-04 14:00:54 -0300

    correcao

---
+++ WebBotLeilao/src/Bot-Leilao.Domain/Entities/Leilao.cs
@@ -0,0 +1,18 @@
+﻿using Bot_Leilao.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Bot_Leilao.Domain
+{
+    [Table("Leiloes")]
+    public class Leilao : BaseEntity<string>
+    {
+        public Leilao()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public DateTime Data { get; set; }
+        public int QuantidadeLotes { get; set; }
+    }
+}
